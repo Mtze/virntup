5c540bedae0fdd527179387e9f0df38c0bfaefec
Matthias Linhuber <matthias.linhuber@tum.de>
2021-04-16 23:08:02 +0200

    [topology_gen] Add two more example topologies

--- topology_generator.py
+++ topology_generator.py
@@ -20,6 +20,39 @@ def create_3_node_topo():
     return topo
 
 
+def create_4_node_topo():
+
+    logging.info("Instantiate new 4 node topology")
+
+    reset_topo_id_counter()
+
+    root = topology.vRouter()
+    topo = topology.V_topology(root)
+
+    tmp = topology.vRouter()
+    root.add_link(tmp)
+    root.add_link(topology.Host())
+    tmp.add_link(topology.Host())
+
+    return topo
+
+
+def create_2_layer_topo():
+    logging.info("Instantiate new multilayer topology")
+
+    reset_topo_id_counter()
+
+    root = topology.vRouter()
+    topo = topology.V_topology(root)
+
+    for i in range(2):
+        l1 = topology.vRouter()
+        root.add_link(l1)
+        for j in range(2):
+            l1.add_link(topology.Host())
+    return topo
+
+
 def create_multi_layer_topo():
     logging.info("Instantiate new multilayer topology")
 
