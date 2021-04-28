4b4e28168bfb7591295b724a4f3d598f0c27fec4
Matthias Linhuber <matthias.linhuber@tum.de>
2021-04-28 17:11:12 +0200

    [codestyle] fix linting errors in target_configurator.py

--- target_configurator.py
+++ target_configurator.py
@@ -2,29 +2,67 @@ import p4runtime_sh.shell as shell
 
 
 class TargetConnector:
+    """TargetConnector.
+
+    Module to connect to an P4 Target. This class assumes that the target either
+    already uses the virntup.p4 program or can be configured with P4Runtime to
+    use virntup.p4.
+
+    """
 
     def __init__(self, target_ip, target_port, p4info_path=None, p4binary_path=None):
+        """__init__.
+
+        Parameters
+        ----------
+        target_ip :
+            str - ip address or hostname of the target
+        target_port :
+            int - P4 Runtime gRPC port used by the target (in most cases this is 50051)
+        p4info_path :
+            (optional) path to p4info file that should be deployed to the target
+        p4binary_path :
+            (optional) p4binary_path path to binary which should be deployed.
+            IMPORTANT: For tofino tagets this binary has to be modified!
+            See -> https://github.com/p4lang/p4runtime-shell#target-specific-support
+        """
         self.target_ip = target_ip
         self.target_port = target_port
 
         if p4info_path is None or p4binary_path is None:
             shell.setup(device_id=0,
-                        grpc_addr=str(self.target_ip) + ":" + str(self.target_port),
+                        grpc_addr=str(self.target_ip) + ":" +
+                        str(self.target_port),
                         election_id=(0, 1)
                         )
-        else: 
+        else:
 
             shell.setup(device_id=0,
-                        grpc_addr=str(self.target_ip) + ":" + str(self.target_port),
+                        grpc_addr=str(self.target_ip) + ":" +
+                        str(self.target_port),
                         election_id=(0, 1),
-                        config=shell.FwdPipeConfig(p4info_path,p4binary_path)
+                        config=shell.FwdPipeConfig(p4info_path, p4binary_path)
                         )
 
-    def insert_route(self,
-                     match_vRouter_number,
+    def insert_route(match_vRouter_number,
                      match_ipv4address,
                      action_dest_mac,
                      action_egress_port):
+        """insert_route.
+
+        Add routing table entry for a specific vRouter
+
+        Parameters
+        ----------
+        match_vRouter_number :
+            int
+        match_ipv4address :
+            string
+        action_dest_mac :
+            string
+        action_egress_port :
+            int
+        """
 
         entry = shell.TableEntry("MyIngress.ipv4NextHopLPM")(
             action="MyIngress.ipv4Forward")
@@ -34,11 +72,21 @@ class TargetConnector:
         entry.action["dstAddr"] = str(action_dest_mac)
         entry.insert()
 
-    def insert_vRouter_port_mapping(self, match_ingress_port, action_vRouter_number):
+    def insert_vRouter_port_mapping(match_ingress_port, action_vRouter_number):
+        """insert_vRouter_port_mapping.
+
+        Assing ingress port to vRouter
+
+        Parameters
+        ----------
+        match_ingress_port :
+            int
+        action_vRouter_number :
+            int
+        """
 
         entry = shell.TableEntry("MyIngress.vSwitchNumberMatching")(
             action="MyIngress.setVSwitchNumber")
         entry.match["standard_metadata.ingress_port"] = str(match_ingress_port)
         entry.action["vSwitchNumberFromTable"] = str(action_vRouter_number)
         entry.insert()
-
